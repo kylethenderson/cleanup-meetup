05531621680f60ed0cebc8e00f7ea36d92cd1bfa
Kyle Henderson <kthenderson83@gmail.com>
2019-07-08 21:13:41 -0500

    update database.sql file with appropriate queries

--- database.sql
+++ database.sql
@@ -1,10 +1,34 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+	"first_name" VARCHAR(128),
+	"last_name" VARCHAR(128),
+	"email" VARCHAR(256),
+	"phone" VARCHAR(16),
+	"admin" BOOLEAN
 );
+	
+CREATE TABLE "pins" (
+	"pin_id" SERIAL PRIMARY KEY,
+	"longitude" VARCHAR(64),
+	"latitude" VARCHAR(64),
+	"description" VARCHAR(512),
+	"ref_created_by" INT REFERENCES "user"
+);
+
+CREATE TABLE "meetups" (
+	"meetup_id" SERIAL PRIMARY KEY,
+	"ref_pin_id" INT REFERENCES "pins",
+	"ref_organized_by" INT REFERENCES "user",
+	"date" DATE,
+	"time" TIME,
+	"supplies" VARCHAR(1024)
+);
+
+CREATE TABLE "meetup_joins" (
+	"join_id" SERIAL PRIMARY KEY,
+	"ref_meetup_id" INT REFERENCES "meetups",
+	"ref_user_id" INT REFERENCES "user"
+);
+
