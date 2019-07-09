ce7d04eee1d822bcc99285bd8dfaa3deeb546413
Kyle Henderson <kthenderson83@gmail.com>
2019-07-08 21:56:52 -0500

    trying to fix heroku

--- server/modules/session-middleware.js
+++ server/modules/session-middleware.js
@@ -2,7 +2,7 @@ const cookieSession = require('cookie-session');
 
 module.exports = cookieSession({
   secret: process.env.SERVER_SESSION_SECRET, // please set this in your .env file
-  key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
+  keys: ['user'], // this is the name of the req.variable. 'user' is convention, but not required
   resave: 'false',
   saveUninitialized: false,
   maxAge: 60 * 60 * 1000, // Set to 1 hour - 60 min/hour * 60 s/min * 1000 ms/s
